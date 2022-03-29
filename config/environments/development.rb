522123f7271f6dddaa78bed74422aad17a3e77e6
yum-n-1211 <naraymk@gmail.com>
2022-03-29 13:34:42 +0000

    [Finish]問い合わせ機能

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,8 +82,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            'naraymk@gmail.com',
-    password:             '',
+    user_name:            ENV['SEND_MAIL'],
+    password:             ENV['SEND_MAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto:  true
   }
