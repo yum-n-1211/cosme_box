83060cd8fb17d75da69182e131ad21b9b9dcbfd7
yum-n-1211 <naraymk@gmail.com>
2022-05-02 06:48:37 +0000

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,10 @@ module CosmeBox
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/models/*.yml').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
-    
-    Rails.application.config.assets.paths << Rails.root.join("vendor", "original_assets", "images")
+
+    Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "images")
+    Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "stylesheets")
+    Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "javascripts")
+
   end
 end
